efc8622452f3423c6fdf38f98d8db96aee2046e4
Zorgos <pierre-louis.sans@edu.devinci.fr>
2024-11-04 16:58:46 +0100

    feat: add addition :sparkles:

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const addBtn = document.getElementById('add');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function addition(a, b) {
+  let c = (a) + (b);
+  return c ;
+};
 
-setupCounter(document.querySelector('#counter'))
+addBtn.addEventListener('onclick', () => {
+  let nbmOne = document.getElementById('inputOne').value;
+  let nbmTwo = document.getElementById('inputTwo').value;
+
+  addition(nbmOne, nbmTwo);
+});
