2283147a08e935c876674da149f766c49844afe3
Nalithdev <Ethan.delcroix@outlook.fr>
2024-11-04 16:30:27 +0100

    test: Create test for multiplication :test_tube:

--- tests/calculatrice.test.js
+++ tests/calculatrice.test.js
@@ -11,3 +11,15 @@ describe('addition test', () => {
         expect(addition(175,32)).toEqual(207)
     })
 })
+
+describe('multiplication test', () => {
+
+    it ('simple multiplication', () => {
+
+        expect(multiplication(1,1)).toEqual(1)
+    })
+
+    it ('multipication a little more harder', () => {
+        expect(multiplication(175,32)).toEqual(5600)
+    })
+})
